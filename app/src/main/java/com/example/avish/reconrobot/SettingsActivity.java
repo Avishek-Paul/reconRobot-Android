dd3fdbf1d27516ff3a44c4df8d2a387067bd7cc4
Avishek Paul <avishek97paul@gmail.com>
2019-04-23 00:30:38 -0400

    Now allows changing of speed through settings activity

--- app/src/main/java/com/example/avish/reconrobot/SettingsActivity.java
+++ app/src/main/java/com/example/avish/reconrobot/SettingsActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    EditText editTextAddress, editTextPort;
+    EditText editTextAddress, editTextPort, editBaseDutyCycle, editReverseDutyCycle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,19 @@ public class SettingsActivity extends AppCompatActivity {
 
         editTextAddress = findViewById(R.id.address);
         editTextPort = findViewById(R.id.port);
+        editBaseDutyCycle = findViewById(R.id.baseDutyCycle);
+        editReverseDutyCycle = findViewById(R.id.reverseDutyCycle);
+
+        String baseDutyCycle = "bdc=" + editBaseDutyCycle.getText().toString();
+        String reverseDutyCycle = "rdc=" + editReverseDutyCycle.getText().toString();
+
+        UdpThread bdcThread = new UdpThread(editTextAddress.getText().toString(), Integer.parseInt(editTextPort.getText().toString()), baseDutyCycle);
+        bdcThread.start();
+        bdcThread.interrupt();
+
+        UdpThread rdcThread = new UdpThread(editTextAddress.getText().toString(), Integer.parseInt(editTextPort.getText().toString()), reverseDutyCycle);
+        rdcThread.start();
+        rdcThread.interrupt();
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("Address", editTextAddress.getText().toString());
